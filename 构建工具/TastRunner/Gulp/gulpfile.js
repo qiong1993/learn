5311d81e9327f7783b741887620253a1d50b2922
qiong1993 <wenqiong.chen@zhaogang.int>
2020-01-14 15:22:58 +0800

    构建工具

--- 构建工具/TastRunner/Gulp/gulpfile.js
+++ 构建工具/TastRunner/Gulp/gulpfile.js
@@ -1,20 +1,5 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
+function defaultTask(cb){
+  cb()
+}
 
-// 代码检查任务 gulp 采取了pipe 方法，用流的方法直接往下传递
-gulp.task('lint', function() {
-  return gulp.src('src/test.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
-// 压缩代码任务
-gulp.task('compress', function() {
-  return gulp.src('src/test.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('build'));
-});
-
-// 将代码检查和压缩组合，新建一个任务
-gulp.task('default', ['lint', 'compress']);
+exports.default = defaultTask
